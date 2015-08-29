dc4387827e87abf267c18fd7ea5cb8a4021d611b
Søren Nørgaard <soerenbnoergaard@gmail.com>
2015-08-29 16:57:52 +0200

    Some commen clean-up

--- projects/dspic33f/adcdac_loopback/adcdacDrv.c
+++ projects/dspic33f/adcdac_loopback/adcdacDrv.c
@@ -17,33 +17,29 @@ static int *buffer[] = {buffer_a, buffer_b}; // Index buffer as buffer[a_or_b][s
  */
 void initAdc(void)
 {
-    AD1CON1bits.FORM = 3; // Data Output Format: Signed Fraction (Q15 format)
-    AD1CON1bits.SSRC = 2; // Sample Clock Source: GP Timer starts conversion
-    AD1CON1bits.ASAM = 1; // ADC Sample Control: Sampling begins immediately after conversion
-    AD1CON1bits.AD12B = 1; // 12-bit ADC operation
+    AD1CON1bits.FORM = 3;  // Signed format, Q15
+    AD1CON1bits.SSRC = 2;  // Timer 3 starts conversion
+    AD1CON1bits.ASAM = 1;  // Auto-start after convertion is finished
+    AD1CON1bits.AD12B = 1; // 12-bit operation
 
     AD1CON2bits.CHPS = 0; // Converts CH0
 
-    AD1CON3bits.ADRC = 0; // ADC Clock is derived from Systems Clock
-    AD1CON3bits.ADCS = 3; // ADC Conversion Clock Tad=Tcy*(ADCS+1)= (1/40M)*4 = 100ns
-    // ADC Conversion Time for 12-bit Tc=14*Tad = 1.4us
+    AD1CON3bits.ADRC = 0; // ADC clock is derived from system clock
+    AD1CON3bits.ADCS = 3; // ADC clock period: TAD = (ADCS+1)*TCY = 100ns
 
-    AD1CON1bits.ADDMABM = 1; // DMA buffers are built in conversion order mode
-    AD1CON2bits.SMPI = 0; // SMPI must be 0
+    AD1CON1bits.ADDMABM = 1; // DMA buffer build mode
+    AD1CON2bits.SMPI = 0; // Increment DMA address after each sampling
 
+    // Select input
+    AD1CHS0bits.CH0SA = 0; // Vin+ = AN0
+    AD1CHS0bits.CH0NA = 0; // Vin- = Vref
 
-    //AD1CHS0: A/D Input Select Register
-    AD1CHS0bits.CH0SA = 0; // MUXA +ve input selection (AN0) for CH0
-    AD1CHS0bits.CH0NA = 0; // MUXA -ve input selection (Vref-) for CH0
-
-    //AD1PCFGH/AD1PCFGL: Port Configuration Register
     AD1PCFGL = 0xFFFF;
-    AD1PCFGLbits.PCFG0 = 0; // AN0 as Analog Input
+    AD1PCFGLbits.PCFG0 = 0; // AN0 as analog input
 
-
-    IFS0bits.AD1IF = 0; // Clear the A/D interrupt flag bit
-    IEC0bits.AD1IE = 0; // Do Not Enable A/D interrupt
-    AD1CON1bits.ADON = 1; // Turn on the A/D converter	
+    IFS0bits.AD1IF = 0;   // Clear the A/D interrupt flag bit
+    IEC0bits.AD1IE = 0;   // Do Not Enable A/D interrupt
+    AD1CON1bits.ADON = 1; // Turn on the A/D converter
 }
 
 /**
