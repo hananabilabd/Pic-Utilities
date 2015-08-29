985545465c8b6535020bd020105f1274f9e1ad96
Søren Nørgaard <soerenbnoergaard@gmail.com>
2015-08-29 19:23:52 +0200

    Still horrible at 1kHz

--- projects/dspic33f/adcdac_loopback/adcdacDrv.c
+++ projects/dspic33f/adcdac_loopback/adcdacDrv.c
@@ -126,7 +126,6 @@ int samplemax = 0;
 void __attribute__((interrupt, no_auto_psv)) _DAC1RInterrupt(void)
 {
     DAC1RDAT = buffer[dmabuffer][sample];
-    /* sample++; */
     if (++sample >= NUMSAMP)
         sample = NUMSAMP-1;
 
